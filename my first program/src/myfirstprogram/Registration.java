22af3f36c64fd9170a01f3edf492cdf720e94125
home <home@LAPTOP-87SI6INJ>
2025-08-04 21:30:08 +0530

    my program

---
+++ my first program/src/myfirstprogram/Registration.java
@@ -0,0 +1,15 @@
+package myfirstprogram;
+
+public class Registration {
+	
+	public void print() {
+        System.out.println("Welcome to Registration!");
+    }
+
+    public static void main(String[] args) {
+        Registration db = new Registration();
+        db.print(); 
+    }
+}
+
+	
